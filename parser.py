1d7ac627fe7e148df680c66fab8ddfa3d23119a6
YellowPig-zp <zipeng.qin.1997@outlook.com>
2018-03-16 23:55:03 -0700

    add some regular expressions for parser

--- parser.py
+++ parser.py
@@ -1,4 +1,14 @@
+import re
+
 class Parser:
+
+    REST = "\\s*(.*)\\s*"
+    COMMA = "\\s*,\\s*"
+    AND = "\\s+and\\s+"
+
+    CREATE_CMD = re.compile("create table " + REST)
+    LOAD_CMD = re.compile("load " + REST)
+    STORE_CMD = re.compile("store " + REST)
     def __init__(self):
         pass
     def
