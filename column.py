c6551534da61c6dd9efc3d2bfa57971bbae24fac
YellowPig-zp <zipeng.qin.1997@outlook.com>
2018-03-18 02:26:15 -0700

    add column.py and remove row.py

---
+++ column.py
@@ -0,0 +1,9 @@
+import types
+
+class Column:
+    def __init__(self, items):
+        assert len(items) > 0
+        self.type = type(items[0])
+
+    def join_column(self, col1, col2):
+        pass
